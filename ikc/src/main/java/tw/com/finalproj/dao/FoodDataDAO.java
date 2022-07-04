d1c702f7167bd7f982ffc059ab4f469cfa965b03
劉冠憶 <108283401+GisLiou@users.noreply.github.com>
2022-07-04 11:47:20 +0800

    Delete ikc directory

--- ikc/src/main/java/tw/com/finalproj/dao/FoodDataDAO.java
+++
@@ -1,8 +0,0 @@
-package tw.com.finalproj.dao;
-
-import tw.com.finalproj.service.domain.FoodDataBean;
-
-public interface FoodDataDAO {
-	public abstract FoodDataBean select(Integer sampleid);
-
-}
--- ikc/src/main/java/tw/com/finalproj/dao/FoodDataRepositor.java
+++
@@ -1,9 +0,0 @@
-package tw.com.finalproj.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.com.finalproj.service.domain.FoodDataBean;
-
-public interface FoodDataRepositor extends JpaRepository<FoodDataBean, Integer> {
-
-}
